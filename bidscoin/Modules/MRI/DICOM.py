047ed050f80da75fada94ad72d621a469e909198
Beliy Nikita <beliy.nikita@outlook.com>
2020-03-31 14:52:11 +0200

    implemented dicom

--- bidscoin/Modules/MRI/DICOM.py
+++ bidscoin/Modules/MRI/DICOM.py
@@ -17,6 +17,7 @@ class DICOM(MRI):
     _type = "DICOM"
 
     __slots__ = ["_DICOM_CACHE", "_DICOMFILE_CACHE", "isSiemens"]
+    __spetialFields = {}
 
     def __init__(self, recording=""):
         super().__init__()
@@ -71,7 +72,6 @@ class DICOM(MRI):
             dicomdict = pydicom.dcmread(path, stop_before_pixels=True)
             self._DICOMFILE_CACHE = path
             self._DICOMDICT_CACHE = dicomdict
-            self.isSiemens = self.is_dicomfile_siemens(self._DICOMFILE_CACHE)
         self.index = index
 
     def acqTime(self) -> datetime:
@@ -79,85 +79,117 @@ class DICOM(MRI):
             dt_stamp = self._DICOM_CACHE["AcquisitionDateTime"]
             return self.__transform(dt_stamp)
         
-        date_stamp = int(self.getField("AcquisitionDate"))
-        time_stamp = float(self.getField("AcquisitionTime"))
+        acq = datetime.min
 
-
-    def get_field(self, field: str):
-        try:
-            value = self._DICOMDICT_CACHE.get(field)
-            if not value:
-                for elem in self._DICOMDICT_CACHE.iterall():
-                    if elem.name == field:
-                        value = elem.value
-                        continue
-        except Exception:
-            try:
-                value = self.parse_x_protocol(field)
-            except Exception:
-                logger.warning("Could not parse {} from {}"
-                               .format(field, self._DICOMFILE_CACHE))
-                value = None
-
-        if not value:
-            return ""
-        elif isinstance(value, int):
-            return int(value)
+        if "AcquisitionDate" in self._DICOM_CACHE:
+            date_stamp = self.__transform(self._DICOM_CACHE["AcquisitionDate"])
         else:
-            return str(value)
+            logger.warning("{}: Acquisition Date not defined"
+                           .format(self.recIdentity()))
+            return acq
+        
+        if "AcquisitionDate" in self._DICOM_CACHE:
+            time_stamp = self.__transform(self._DICOM_CACHE["AcquisitionTime"])
+        else:
+            logger.warning("{}: Acquisition Time not defined"
+                           .format(self.recIdentity()))
+            return acq
+        acq = datetime.combine(date_stamp, time_stamp)
+        return acq
 
-    def parse_x_protocol(self, pattern: str) -> str:
-        """
-        Siemens writes a protocol structure as text into each DICOM file.
-        This structure is necessary to recreate a scanning protocol
-        from a DICOM, since the DICOM information alone
-        wouldn't be sufficient.
+    def dump(self):
+        if self._DICOMDICT_CACHE is not None:
+            return str(self._DICOM_CACHE)
+        elif len(self.files) > 0:
+            self.loadFile(0)
+            return str(self._DICOM_CACHE)
+        else:
+            logger.error("No defined files")
+            return "No defined files"
 
-        :param pattern:     A regexp expression:
-                            '^' + pattern + '\t = \t(.*)\\n'
-        :return:            The string extracted values from the dicom-file
-                            according to the given pattern
-        """
-        if not self.isSiemens:
-            logger.warning('Parsing {} may fail because {} does not seem '
-                           'to be a Siemens DICOM file'
-                           .format(pattern, self.file))
-        regexp = '^' + pattern + '\t = \t(.*)\n'
-        regex = re.compile(regexp.encode('utf-8'))
-        with open(self.file, 'rb') as openfile:
-            for line in openfile:
-                match = regex.match(line)
-                if match:
-                    return match.group(1).decode('utf-8')
-        logger.warning("Pattern: '{}' not found in {}"
-                       .format(regexp.encode('unicode_escape').decode(),
-                               self.file))
-        return None
+    def _getField(self, field: list):
+        res = None
+        try:
+            if field[0] in self.__specialFields:
+                res = self._adaptMetaField(field[0])
+            else:
+                value = self._DICOM_CACHE
+                for f in field:
+                    if isinstance(value, pydicom.dataset.Dataset):
+                        value = value[f]
+                    if isinstance(value, pydicom.dataelem.DataElement):
+                        if value.VR == "SQ":
+                            value = value[int(f)]
+                        else:
+                            value = self.__transform(value)
+                            break
+                res = value
+        except Exception:
+            logger.warning("{}: Could not parse '{}'"
+                           .format(self.recIdentity(), field))
+            res = None
+        return res
 
-    def is_dicomfile_siemens(file: str) -> bool:
-        """
-        Checks whether a file is a *SIEMENS* DICOM-file.
-        All Siemens Dicoms contain a dump of the  MrProt structure.
-        The dump is marked with a header starting with 'ASCCONV BEGIN'.
-        Though this check is not foolproof, it is very unlikely to fail.
+    def recNo(self):
+        return self.getField("SeriesNumber", 0)
 
-        :param file:    The full pathname of the file
-        :return:        Returns true if a file is a Siemens DICOM-file
-        """
-        return b'ASCCONV BEGIN' in open(file, 'rb').read()
+    def recId(self):
+        seriesdescr = self.getField("SeriesDescription")
+        if seriesdescr is None:
+            seriesdescr = self.getField("ProtocolName")
+        if seriesdescr is None:
+            logger.warning("{}: Unable to get recording Id for file {}"
+                           .format(self.formatIdentity(),
+                                   self.currentFile()))
+            seriesdescr = "unknown"
+        return seriesdescr.strip()
 
-    def isComplete(self):
-        nrep = self.get_field('lRepetitions')
-        nfiles = self.get_nFiles()
-
-        if nrep and nrep > nfiles:
-            logger.warning('{}: Incomplete acquisition: '
-                           '\nExpected {}, found {} dicomfiles'
-                           .format(self._DICOMFILE_CACHE, nrep, nfiles))
-            return False
+    def isCompleteRecording(self):
         return True
 
-    
+    def _transformField(self, value, prefix: str):
+        if prefix.startswith("time"):
+            exp = prefix[len("time"):]
+            if exp:
+                exp = int(exp)
+            else:
+                exp = 3
+            return value / 10 ** exp
+        elif prefix == "":
+            return value
+        else:
+            logger.warning("{}/{}: Unknown field prefix {}"
+                           .format(self.formatIdentity(),
+                                   prefix))
+ 
+    def clearCache(self) -> None:
+        del self._DICOM_CACHE
+        self._DICOM_CACHE = None
+        self._DICOMFILE_CACHE = ""
+
+    def copyRawFile(self, destination: str) -> None:
+        if os.path.isfile(os.path.join(destination,
+                                       self.currentFile(True))):
+            logger.warning("{}: File {} exists at destination"
+                           .format(self.recIdentity(),
+                                   self.currentFile(True)))
+        shutil.copy2(self.currentFile(), destination)
+        shutil.copy2(tools.change_ext(self.currentFile(), "json"),
+                     destination)
+
+    def _getSubId(self) -> str:
+        return str(self.getField("PatientID"))
+
+    def _getSesId(self) -> str:
+        return ""
+
+
+    ########################
+    # Additional fonctions #
+    ########################
+    def _adaptMetaField(self, name):
+        return None
+
     def __transform(element: pydicom.dataelem.DataElement):
         if element is None:
             return None
@@ -165,181 +197,98 @@ class DICOM(MRI):
         VM = element.VM
         val = element.value
 
-        if VR == "AE":
-            """
-            Application Entity
-                A string of characters that identifies an Application Entity
-                with leading and trailing spaces (20H) being non-significant.
-                A value consisting solely of spaces shall not be used.
-            """
-            return str(value.value)
-        elif VR == "AS":
-            """
-            Age String
-                A string of characters with one of the following formats -- 
-                nnnD, nnnW, nnnM, nnnY; where nnn shall contain 
-                    the number of days for D, 
-                    weeks for W, 
-                    months for M, 
-                    or years for Y.
+        if VM > 1:
+            return [self.__decodeValue(val[i], VR) for i in range(VM)]
+        else:
+            return self.__decodeValue(val[i], VR)
 
-            Example: "018M" would represent an age of 18 months.
-            """
-            if val.endswith("Y"):
-                return int(val[:-1])
-            elif val.endswith("M"):
-                return int(val[:-1])
-            elif val.endswith("W"):
-                return int(val[:-1])
-            elif val.endswith("W"):
+    def __decodeValue(self, val, VR: str):
+        """
+        Decodes a value from pydicom.DataElement to corresponding
+        python class following description in:
+        http://dicom.nema.org/medical/dicom/current/output/
+        chtml/part05/sect_6.2.html
+    
+        Nested values are not permitted
+
+        Parameters
+        ----------
+        val: 
+            value as stored in DataElements.value
+        VR: str
+            Value representation defined by DICOM
+
+        Returns
+        -------
+            decoded value
+        """
+
+        # Byte Numbers:
+        # parced by pydicom, just return value
+        if VR in ("FL", "FD",
+                  "SL", "SS", "SV",
+                  "UL", "US", "UV"):
+            return val
+
+        # Text Numbers
+        # using int(), float()
+        if VR == "DS":
+            return float(val)
+        if VR == "IS":
+            return int(val)
+
+        # Text and text-like
+        # using strip to remove apddings
+        if VR in ("AE", "CS", "PN", 
+                  "LO", "LT",
+                  "SH", "UC",
+                  "UR", "UT", "UI"):
+            return val.strip(" \0")
+
+        # Age string 
+        # unit mark is ignored, value converted to int
+        if VR == "AS":
+            if val[-1] in "YMWD":
                 return int(val[:-1])
             else:
                 return int(val)
-        elif VR == "AT":
-            """
-            Attribute Tag
-                Ordered pair of 16-bit unsigned integers that is the value
-                of a Data Element Tag.
 
-                Example: A Data Element Tag of (0018,00FF) would be encoded
-                as a series of 4 bytes in a Little-Endian Transfer Syntax as
-                18H,00H,FFH,00H.
-            """
-            logger.warning("Tag {}({}) contains DataElement"
-                           .format(element.name, element.tag))
-            return None
-        elif VR == "CS":
-            """
-            Code String
-                A string of characters identifying a controlled concept.
-                Leading or trailing spaces (20H) are not significant.
-            """
-            return val
-        elif VR == "DA":
-            """
-            Date
-                A string of characters of the format YYYYMMDD;
-                where YYYY shall contain year,
-                MM shall contain the month,
-                and DD shall contain the day,
-                interpreted as a date of the Gregorian calendar system.
-            """
-            return datetime.strptime(val, "%Y%m%d")
-        elif VR == "DS":
-            """
-            Decimal String
-                A string of characters representing either 
-                a fixed point number or a floating point number.
-                A fixed point number shall contain only the characters 
-                0-9 with an optional leading "+" or "-" and an optional "."
-                to mark the decimal point.
-                A floating point number shall be conveyed as defined 
-                in ANSI X3.9, with an "E" or "e" to indicate the start
-                of the exponent. 
-                Decimal Strings may be padded with leading or trailing spaces. 
-                Embedded spaces are not allowed.
-            """
-            return float(val)
-        elif VR == "DT":
-            """
-            Date Time
-                A concatenated date-time character string in the format:
-                YYYYMMDDHHMMSS.FFFFFF&ZZXX
-                
-                The components of this string, from left to right, are
-                YYYY = Year, MM = Month, DD = Day,
-                HH = Hour (range "00" - "23"),
-                MM = Minute (range "00" - "59"),
-                SS = Second (range "00" - "60").
-                FFFFFF = Fractional Second contains a fractional part of
-                a second as small as 1 millionth of a second
-                (range "000000" - "999999").
-
-                &ZZXX is an optional suffix for offset from 
-                Coordinated Universal Time (UTC), 
-                where & = "+" or "-", 
-                and ZZ = Hours and XX = Minutes of offset.
-            """
-            val = val.strip()
-            time_stamp = None
-            dt = timedelta()
-            if val[-5] == "+":
-                dt = timedelta(hours=int(val[-4:-2],
-                               minutes=int(val[-2:])))
-                val = val[0:-5]
-            if val[-5] == "-":
-                dt = -timedelta(hours=int(val[-4:-2],
-                                minutes=int(val[-2:])))
-                val = val[0:-5]
+        # Date and time
+        # converted to corresponding datetime subclass
+        if VR == "TM":
             if "." in val:
-                time_stamp = datetime(val, "%Y%m%d%H%M%S.%f")
+                return time.strptime(val, "%H%M%S.%f")
             else:
-                time_stamp = datetime(val, "%Y%m%d%H%M%S")
-            return time_stamp + dt
-        elif VR == "FL":
-            """
-            Floating Point Single
-            """
-            return val
-        elif VR == "FD":
-            """
-            Floating Point Double
-            """
-            return val
-        elif VR == "IS":
-            """
-            Integer String
-            """
-            return int(val)
-        elif VR == "LO"
-            """
-            Long String
-            """
-            return val
-        elif VR == "LT":
-            """
-            Long Text
-            """
-            return val
-        elif VR == "OB":
-            """
-            Other Byte
-            """
-            logger.warning("Tag {}({}) contains Transfert syntax"
-                           .format(element.name, element.tag))
+                return time.strptime(val, "%H%M%S")
+        if VR == "DA":
+            return date.strptime(val, "%Y%m%d")
+        if VR == "DT":
+            val = val.strip()
+            date_string = "%Y%m%d"
+            time_string = "%H%M%S"
+            if "." in val:
+                time_string += ".%f"
+            if "+" in val or "-" in val:
+                time_string += "%z"
+            t = datetime.strptime(val, date_string + time_string)
+            if t.tzinfo is not None:
+                t += t.tzinfo.utcoffset(t)
+                t = t.replace(tzinfo=None)
+            return t
+
+        # Invalid type
+        # Attributes and sequences will produce warning and return
+        # None
+        if VR in ("AT", "SQ", "UN"):
+            logger.warning("Invalid VR: {}".format(VR))
             return None
-        elif VR == "OD":
-            """
-            Other double
-            """
-            logger.warning("Tag {}({}) contains Transfert syntax"
-                           .format(element.name, element.tag))
+
+        # Other type
+        # Not clear how parce them
+        if VR in ("OB", "OD", "OF", "OL", "OV", "OW"):
+            logger.warning("Other VR: {}".format(VR))
             return None
-        elif VR == "OF":
-            """
-            Other double
-            """
-            logger.warning("Tag {}({}) contains Transfert syntax"
-                           .format(element.name, element.tag))
-            return None
-        elif VR == "OL":
-            """
-            Other double
-            """
-            logger.warning("Tag {}({}) contains Transfert syntax"
-                           .format(element.name, element.tag))
-            return None
-        elif VR == "OV":
-            """
-            Other double
-            """
-            logger.warning("Tag {}({}) contains Transfert syntax"
-                           .format(element.name, element.tag))
-            return None
-        elif VR == "OW":
-            """
-            Other double
-            """
-            logger.warning("Tag {}({}) contains Transfert syntax"
-                           .format(element.name, element.tag))
-            return None
+
+        # unregistered VR
+        logger.error("{} is not valid DICOM VR")
+        raise ValueError("invalid VR")
