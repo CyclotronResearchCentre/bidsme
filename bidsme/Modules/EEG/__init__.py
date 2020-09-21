7298e8567bfc619cf58d4e1ebbb68a6a0e6f244d
Beliy Nikita <beliy.nikita@outlook.com>
2020-09-21 15:31:16 +0200

    fixed import issue

--- bidsme/Modules/EEG/__init__.py
+++ bidsme/Modules/EEG/__init__.py
@@ -3,6 +3,15 @@
 from .EEG import channel_types
 
 from .BrainVision import BrainVision
-from .EDF import EDF
 
-__all__ = ["BrainVision", "EDF"]
+try:
+    from .EDF import EDF
+except ModuleNotFoundError as e:
+    if e.name == "mne":
+        from .._formats.dummy import dummy as EDF
+        EDF.classes["EDF"] = e.name
+    else:
+        raise
+
+
+__all__ = ["BrainVision", "EDF", "channel_types"]
