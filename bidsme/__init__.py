1e5c3229d6d2beffb92ca5fd125c9824304cadd0
Beliy Nikita <beliy.nikita@uliege.be>
2025-05-07 13:04:04 +0200

    Fixed GUI import

--- bidsme/__init__.py
+++ bidsme/__init__.py
@@ -1,11 +1,11 @@
-from .main import init, main
+from .main import init, main, GUI
 from .prepare import prepare
 from .mapper import mapper
 from .process import process
 from .bidsify import bidsify
 
 __all__ = ["init", "main", "prepare",
-           "mapper", "process", "bidsify"
-           ]
+           "mapper", "process", "bidsify",
+           "GUI"]
 
 name = "bidsme"
